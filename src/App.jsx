5d782393ce3ed084f17ab454e48ea9974ad2909d
PhongNguyen-72 <194211344+PhongNguyen-72@users.noreply.github.com>
2026-06-24 10:08:30 +0700

    Remove unused JSX components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,3 @@
-import Header from "./Header";
-import Footer from "./Footer";
-import Food from "./Food";
-import Card from "./Card";
-import Button from "./Button";
-import Button1 from "./Button1/Button1.jsx";
-import Button2 from "./Button2.jsx";
-import Student from "./Student.jsx";
-import UserGreeting from "./UserGreeting.jsx";
-import List from "./List.jsx";
-
 function App() {
   const vegetables = [
     { id: 1, name: "Broccoli", calories: 50 },
@@ -16,28 +5,7 @@ function App() {
   ];
   return (
     <div>
-      <Header />
-      <Food />
-      <Card />
-      <Card />
-      <Card />
-      <Footer />
-      <Button />
-      <Button1 />
-      <Button2 />
-      <div>
-        <Student name="Alice" age={20} isStudent={true} />
-        <Student name="Bob" age={22} isStudent={false} />
-        <Student name="Alex" age={52} />
-        <Student />
-      </div>
-      <div>
-        <UserGreeting isLoggedIn={true} />
-        <UserGreeting isLoggedIn={false} />
-      </div>
-      <div>
-        <List item={vegetables} />
-      </div>
+      <h2>Hello ! My name is Phong</h2>
     </div>
   );
 }
